d53335a561d605cf324bf1192546876821a146f9
Leo Laporte <leo@leoville.com>
2022-01-19 09:37:17 -0800

    Day 15 works - but is too slow.

--- AOC21/day15.rkt
+++ AOC21/day15.rkt
@@ -13,7 +13,8 @@ What is the lowest total risk of any path from the top left to the bottom right?
 
 (require rackunit
          threading
-         racket/trace)
+         racket/trace
+         racket/set) 
 
 #|==============================================================================|#
 #|                                      DATA                                    |#
@@ -88,12 +89,14 @@ function.
   (let-values ([(y x) (quotient/remainder p (grid-width g))])
     (cons x y)))
 
-(define (value p g)
+;; Natural Grid -> Natural
+;; returns the value of a point in a grid
+(define (get-value p g)
   (vector-ref (grid-points g) p))
 
 ;; Natural Grid -> (list-of Natural)
 ;; given a point on a grid structure, return a list of the 
-;; surrounding points 
+;; surrounding points - with lowest risk points first
 (define (look-around p g)
   (let* ([pos (point->pos p g)]
          [x (car pos)]
@@ -104,35 +107,58 @@ function.
          [r (cons (add1 x) y)]
          [u (cons x (sub1 y))]
          [d (cons x (add1 y))])
+    
     (define (in-grid? xy) (and (< -1 (car xy) width) (< -1 (cdr xy) height)))
 
+    (define (by-value x y)   ; sort surrounding points from lowest to highest risk
+      (< (get-value x g) (get-value y g)))
+    
     (~>  (list u l r d)               ; the list of points as (x . y)
          (filter in-grid? _)          ; remove points that are off the grid
-         (map (λ (x) (pos->point (car x) (cdr x) g)) _)))) ; convert (x . y) back to vector-ref
+         (map (λ (x) (pos->point (car x) (cdr x) g)) _) ; convert (x . y) back to vector-ref
+         (sort _ by-value))))         ; lowest risk points first
 
-(define (day15.1 g)
-  (define end (sub1 (vector-length (grid-points g))))    ; the destination point, last point on grid
+(define (get-max-risk g)
+  "get a reasonable starting point for the max-risk parameter - L-shaped route"
+  (define end (sub1 (vector-length (grid-points g))))
+  (define end-x (car (point->pos end g)))
+  (define end-y (cdr (point->pos end g)))
+  (define width (grid-width g))
+  
+  (+ (for/sum ([y (in-range (add1 end-y))])
+       (get-value (pos->point 0 y g) g))
+
+     (for/sum ([x (in-range (add1 end-x))])
+       (get-value (pos->point x end-y g) g))))
     
-  (define (next-step p g risk max-risk visited)
- ;  (printf "Point: ~a - Risk: ~a - Max: ~a\nVisited: ~a\n\n\n" p risk max-risk visited)
-    (cond [(equal? p end) (set! max-risk risk) max-risk] ; at the end, return the risk, make it max
-          [(>= risk max-risk) risk]                      ; stop when max is exceded
-          [(not (false? (member p visited))) max-risk]   ; don't revisit points
-          [else (set! risk (+ risk (value p g)))         ; point is ok, add value to risk
-                (set! visited (cons p visited))          ; add point to visited
-                (next-steps (look-around p g) g risk max-risk visited)])) ; look for next move
+(define (day15.1 g)
+  (define end (sub1 (vector-length (grid-points g)))) ; destination: last point on grid
+  (define max-risk (get-max-risk g))      ; start with a reasonable max (L-shaped path)
+    
+  (define (next-step p g risk max-risk seen)          ; check current point on grid
+    (printf "Point: ~a - Risk: ~a - Max: ~a - Seen: ~a\n\n" p risk max-risk seen)
+    (cond [(equal? p end) (set! max-risk risk) risk]  ; end, make risk new max, return it
+          [(>= risk max-risk) risk]                   ; this path is worse - abandon
+          [(set-member? seen p) max-risk]             ; been here already - don't repeat
+          [else
+               (next-steps                            ; go on with next points on grid
+                (look-around p g)                     ; get next points sorted by risk
+                g                                     ; where are we again? oh yeah, grid
+                (+ risk (get-value p g))              ; keeping track of path's total risk
+                max-risk                              ; keep track of best path so far
+                (set-add seen p))]))                  ; adding this point to seen
   
-    (define (next-steps lop g risk max-risk visited)
-      (cond [(empty? lop) max-risk]
-            [else (min (next-step (first lop) g risk max-risk visited)
-                       (next-steps (rest lop) g risk max-risk visited))]))
+  (define (next-steps lop g risk max-risk seen)
+    (cond [(empty? lop) max-risk]
+          [else (min (next-step (first lop) g risk max-risk seen)
+                     (next-steps (rest lop) g risk max-risk seen))]))
   
-  (next-step 0 g 0 41 empty))
+  (next-step 0 g 0 max-risk (set empty)))
 
 (module+ test
-  (check-equal? (day15.1 test-grid) 40))
+  (time (check-equal? (day15.1 test-grid) 40)))
 
-; (time (printf "2021 AOC Problem 15.1 = ~a\n" (day15.1 input)))
+; (time (printf "2021 AOC Problem 15.1 = ~a\n" (day15.1 input-grid)))
 
 #|=================================================================================
                                         PART 2
