c8ffc901f2a8a196ea4fcb6e6b3ed6192813a78d
Leo Laporte <leo@leoville.com>
2022-01-25 21:20:50 -0800

    Day 16.1 working on bits->xp

--- AOC21/day16.rkt
+++ AOC21/day16.rkt
@@ -20,26 +20,38 @@ get if you add up the version numbers in all packets?
 #|                                      DATA                                    |#
 #|==============================================================================|#
 
-(define (parse-input str)
-  
-  (define (hex->bits hex)
-    (match hex
-      [#\0 '(0 0 0 0)] [#\1 '(0 0 0 1)] [#\2 '(0 0 1 0)] [#\3 '(0 0 1 1)]
-      [#\4 '(0 1 0 0)] [#\5 '(0 1 0 1)] [#\6 '(0 1 1 0)] [#\7 '(0 1 1 1)]
-      [#\8 '(1 0 0 0)] [#\9 '(1 0 0 1)] [#\A '(1 0 1 0)] [#\B '(1 0 1 1)]
-      [#\C '(1 1 0 0)] [#\D '(1 1 0 1)] [#\E '(1 1 1 0)] [#\F '(1 1 1 1)]
-      [#\newline '()]))
+(require data/queue) ; for queue structure
 
-  (~> str
-      string->list
-      (map hex->bits _)
-      flatten))
+;; String -> Queue
+(define (input->queue str)
+  "takes input as a string of hex digits and turns it into a queue of bits"
+  (let ([q (make-queue)])
+    
+    (define (hex->bits hex)
+      (match hex
+        [#\0 '(0 0 0 0)] [#\1 '(0 0 0 1)] [#\2 '(0 0 1 0)] [#\3 '(0 0 1 1)]
+        [#\4 '(0 1 0 0)] [#\5 '(0 1 0 1)] [#\6 '(0 1 1 0)] [#\7 '(0 1 1 1)]
+        [#\8 '(1 0 0 0)] [#\9 '(1 0 0 1)] [#\A '(1 0 1 0)] [#\B '(1 0 1 1)]
+        [#\C '(1 1 0 0)] [#\D '(1 1 0 1)] [#\E '(1 1 1 0)] [#\F '(1 1 1 1)]
+        [#\newline '()]))
 
-(define input (parse-input (file->string "input16.txt")))
-(define test-1 (parse-input "8A004A801A8002F478"))
-(define test-2 (parse-input "620080001611562C8802118E34"))
-(define test-3 (parse-input "C0015000016115A2E0802F182340"))
-(define test-4 (parse-input "A0016C880162017C3686B18A3D4780"))
+    (define bit-list
+      (~> str
+          string->list
+          (map hex->bits _)
+          flatten))
+
+    (for ([bit (in-list bit-list)])
+      (enqueue! q bit))
+
+    q))
+
+(module+ test
+  (check-equal? (queue->list (input->queue "F")) '(1 1 1 1))
+  (check-equal? (queue->list (input->queue "DF")) '(1 1 0 1 1 1 1 1))
+  (check-equal? (queue->list (input->queue "13")) '(0 0 0 1 0 0 1 1))
+  )
+
 
 #|==============================================================================|#
 #|                                     NOTES                                    |#
@@ -63,14 +75,18 @@ Bits
 * Literal packets begin with a 1 until the last packet which begins with a 0 - so each
 literal packet is 5 bits
 
+"The hexadecimal representation of this packet might encode a few extra 0 bits at the end;
+these are not part of the transmission and should be ignored." So after processing
+discard extra 0 bits until you reach a border. TKTKTK
+
 So using these rules (and considering what I might want to do in part 2) I think
 the best way to solve part 1 is to create a function that takes the input as
 a stream of bits and turns it into a list of expressions. For part one
 I can just sum the version numbers of each item in the list.
 
-(flatten input) gives us a stream of bits. racket/stream gives us (first stream)
-and (rest stream). So I can make an assembly line which assembles the streams
-into expression structures I'll call xp.
+For efficiency I'll store the bits in a vector, and use a pointer to show
+my current position in the stream. So I can make an assembly line which assembles
+the bits into a list of expression structures.
 
 |#
 
@@ -78,80 +94,82 @@ into expression structures I'll call xp.
 #|                                     CODE                                     |#
 #|==============================================================================|#
 
-(require racket/stream)
-
-(struct xp (version op data))
+(struct xp (version op data) #:transparent)
 ;; xp is (xp Natural Natural Natural)
 ;; where version is a number from 0-7
 ;; op is a number from 0-7
-;; and data is a Natural
+;; and data is a Natural or (list-of xp)
 
-(define (process-stream st)
-  (for/list ))
+;; (queue-of 1 or 0) -> Xp
+;;; Given a stream of bits render it an expression Xp
+(define (bits->xp q)
+  (let* ([version (take 3 q)]
+         [op (take 3 q)]
+         [data
+          (cond [(equal? op 4) (get-literal q)]
+                [else (if (equal? 0 (take 1 q)) ; length type ID
+                          (get-data q)
+                          (bits->xp q))])])
+ 
+    ; dump padding to get to start of next expression
 
-;; Stream -> Xp
-;; Given a stream of bits render it an expression Xp
-(define (stream->xp stream)
-  (define version (bin->decimal (take 3 stream)))
-  (define op (bin->decimal (take 3 stream)))
-
-  (cond [(equal? op 4) (define data (get-literal stream))]
-        [else (if (equal? 0 (take 1 stream))
-                 (define data (get-data stream))
-                 (define data (xp-data (stream->xp stream))))])
-
-  ; dump padding to get to start of next expression
-
-  (xp version op data))
+    (xp version op data)))
 
 (module+ test
-  (check-equal? (stream->xp '(110100101111111000101000)) (xp 6 4 2021))
-  (check-equal? (stream->xp '(00111000000000000110111101000101001010010001001000000000))
-                (xp 1 6 '(10 20)))
-  (check-equal? (stream->xp '(11101110000000001101010000001100100000100011000001100000))
-                (xp 7 3 '(1 2 3))))
-
-;; Natural Stream -> Natural
-;; Take count bits off stream and convert them into a number
-;; !!!
-(define (take cnt st) 0) ; stub
-
-;; Stream -> Natural
-;; given a stream use the literal rules to produce a number
-;; !!!
-(define (get-literal st) 0 ) ; stub
-
-;; Stream -> (list-of Natural)
-;; given a stream use the literal rules to produce a list
-;; of numbers
-;; !!!
-(define (get-data st) 0 ) ; stub
-
-;; Stream -> (list-of Natural)
-;; given a stream use the literal rules to produce a list
-;; of numbers
-;; !!!
-(define (get-sub-packets st) 0 ) ; stub
+  (check-equal? (bits->xp (input->queue "D2FE28")) (xp 6 4 2021))
+  (check-equal? (bits->xp (input->queue "38006F45291200")) (xp 1 6 '((xp 6 4 10) (xp 2 4 20))))
+  (check-equal? (bits->xp (input->queue "EE00D40C823060")) (xp 7 3 '((xp 2 4 1) (xp 4 4 2) (xp 1 4 3)))))
+  )
 
 ;; (list-of 1 or 0) -> natural
 ;; turn list of binary digits into its decimal equivalent, eg. '(1 0 1 0) into 10
+;; reused from Day 3
 (define (bin->decimal lst)
   (local [(define (list->string l)
             (cond ((empty? l) "")
                   (else (string-append (number->string (first l)) (list->string (rest l))))))]
     (string->number (list->string lst) 2)))
 
+(module+ test
+  (check-equal? (bin-list->decimal '(1 0 1 0)) 10)
+  (check-equal? (bin-list->decimal '(1 1 1 1)) (+ 1 2 4 8))
+  (check-equal? (bin-list->decimal '(1 0 0 0)) 8))
 
-
-(define (day16.1 l) 0) ; stub
+;; Natural Stream -> Natural
+;; Take count bits off stream and convert them into a number
+(define (take cnt q)
+  (bin->decimal
+   (for/list ([i (in-range cnt)])
+     (dequeue! q))))
 
 (module+ test
-  (check-equal? (day16.1 test-1) 16)
-  (check-equal? (day16.1 test-2) 12)
-  (check-equal? (day16.1 test-3) 23)
-  (check-equal? (day16.1 test-4) 21))
+  (check-equal? (take 3 (input->queue "D2FE28")) 6)
+  (check-equal? (take 3 (input->queue "38006F45291200")) 1)
+  (check-equal? (take 3 (input->queue "EE00D40C823060")) 7)
+  )
 
-; (time (printf "2021 AOC Problem 16.1 = ~a\n" (day16.1 input)))
+;; Stream -> Natural
+;; given a stream use the literal rules to produce a number
+;; !!!
+(define (get-literal q) 0 ) ; stub
+
+;; Stream -> (list-of Natural)
+;; given a stream use the literal rules to produce a list
+;; of numbers
+;; !!!
+(define (get-data q) 0 ) ; stub
+
+
+#|
+(module+ test
+  (check-equal? (day16.1 (input->queue "8A004A801A8002F478")) 16)
+  (check-equal? (day16.1 (input->queue "620080001611562C8802118E34")) 12)
+  (check-equal? (day16.1 (input->queue "C0015000016115A2E0802F182340")) 23)
+  (check-equal? (day16.1 (input->queue "A0016C880162017C3686B18A3D4780")) 21))
+
+(time (printf "2021 AOC Problem 16.1 = ~a\n" (day16.1 (file->string "input16.txt"))))
+
+|#
 
 #|=================================================================================
                                         PART 2
@@ -159,10 +177,12 @@ into expression structures I'll call xp.
 
 ==================================================================================|#
 
-;(module+ test
-;  (check-equal? (day16.2 test-data) 0))
+#|
+(module+ test
+  (check-equal? (day16.2 test-data) 0))
 
-; (time (printf "2021 AOC Problem 16.2 = ~a\n" (day16.2 input)))
+ (time (printf "2021 AOC Problem 16.2 = ~a\n" (day16.2 input)))
+|#
 
 #|
 Time to solve, in milliseconds, on a 2021 M1 Pro MacBook Pro 14" with 16GB RAM
