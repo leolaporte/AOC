208b61d91c064ad14d18277b98841cea01643a34
Leo Laporte <leo@leoville.com>
2022-02-12 10:25:59 -0800

    Part 1 solved, Part 2 very very close - just need to calculate ranges

--- AOC21/day17.rkt
+++ AOC21/day17.rkt
@@ -121,25 +121,27 @@ area after any step?
 ;; Probe -> Probe
 ;; given a probe position return the next position according to the rules provided
 (define (step p)
-    (probe
-     ; new x-pos
-     (if (<= (probe-x p) 0)            ; velocity has bottomed out
-         0                             ; so we ain't movin'
-         (+ (probe-x p) (probe-xv p))) ; otherwise move by x velocity
+  (probe
+   ; new x-pos
+   (if (<= (probe-xv p) 0)           ; velocity has bottomed out
+       (probe-x p)                   ; so we ain't movin'
+       (+ (probe-x p) (probe-xv p))) ; otherwise move by x velocity
 
-     ; new y-pos 
-     (+ (probe-y p) (probe-yv p))
+   ; new y-pos 
+   (+ (probe-y p) (probe-yv p))
        
-     ; new x-velocity
-     (sub1 (probe-xv p))
+   ; new x-velocity
+   (if (> (probe-xv p) 0)
+       (sub1 (probe-xv p))
+       0)
      
-     ; new y-velocity
-     (sub1 (probe-yv p))))
+   ; new y-velocity
+   (sub1 (probe-yv p))))
 
 (module+ test
-  (check-equal? (step (probe 0 0 0 0)) (probe 0 0 -1 -1))
+  (check-equal? (step (probe 0 0 0 0)) (probe 0 0 0 -1))
   (check-equal? (step (probe 1 2 7 8)) (probe 8 10 6 7))
-  (check-equal? (step (probe 0 -2 7 8)) (probe 0 6 6 7))
+  (check-equal? (step (probe 0 -2 7 8)) (probe 7 6 6 7))
   (check-equal? (step (probe 1 -2 7 -8)) (probe 8 -10 6 -9)))
     
 ;; Probe Target -> Boolean
@@ -167,12 +169,53 @@ area after any step?
 ;; Given a probe return #t if it hits the target 
 ;; #f if it misses
 (define (flight p t)
-  (cond [(hit-target? p) #t]
+  (cond [(hit-target? p t) #t]
         [(not (in-range? p t)) #f]
         [else (flight (step p) t)]))
 
-;(module+ test
-;  (check-equal? (day17.2 "target area: x=20..30, y=-10..-5") 112))
+#|
+ (trace flight)
+ (flight (probe 0 0 7 8) (make-target "target area: x=20..30, y=-10..-5"))
+
+(regexp-match* #px"(-?\\d+),(-?\\d+)" str)
+
+"23,-10  25,-9   27,-5   29,-6   22,-6   21,-7   9,0     27,-7   24,-5
+25,-7   26,-6   25,-5   6,8     11,-2   20,-5   29,-10  6,3     28,-7
+8,0     30,-6   29,-8   20,-10  6,7     6,4     6,1     14,-4   21,-6
+26,-10  7,-1    7,7     8,-1    21,-9   6,2     20,-7   30,-10  14,-3
+20,-8   13,-2   7,3     28,-8   29,-9   15,-3   22,-5   26,-8   25,-8
+25,-6   15,-4   9,-2    15,-2   12,-2   28,-9   12,-3   24,-6   23,-7
+25,-10  7,8     11,-3   26,-7   7,1     23,-9   6,0     22,-10  27,-6
+8,1     22,-8   13,-4   7,6     28,-6   11,-4   12,-4   26,-9   7,4
+24,-10  23,-8   30,-8   7,0     9,-1    10,-1   26,-5   22,-9   6,5
+7,5     23,-6   28,-10  10,-2   11,-1   20,-9   14,-2   29,-7   13,-3
+23,-5   24,-8   27,-9   30,-7   28,-5   21,-10  7,9     6,6     21,-5
+27,-10  7,2     30,-9   21,-8   22,-7   24,-9   20,-6   6,9     29,-5
+8,-2    27,-8   30,-5   24,-7"
+|#
+#|
+So yeah, everything I wrote in part 1 is still good here. The question remains,
+what should the range of values for xv and yv be? In the sample problem the range
+is xv: 6..30, yv: 10...-7 - I could just guess.
+|#
+
+;; Natural Natural Natural Natural Target -> Natural
+;; given the minimum and maximum velocities for x and y
+;; return the number of successful flights
+(define (test-flights xv-min xv-max yv-min yv-max t)
+  (for*/fold ([hits 0])
+            ([xv (in-range xv-min (add1 xv-max))]
+             [yv (in-range yv-min (add1 yv-max))])
+    (values (+ hits
+               (if (flight (probe 0 0 xv yv) t)
+                   1
+                   0)))))
+
+(define (day17.2 str)
+  (test-flights 0 100 -100 100 (make-target str)))
+
+(module+ test
+  (check-equal? (day17.2 "target area: x=20..30, y=-10..-5") 112))
 
 ; (time (printf "2021 AOC Problem 17.2 = ~a\n" (day16.2 input)))
 
