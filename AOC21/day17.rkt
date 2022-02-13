bcd5f08215030ffc0bc191d0d31a96471bf84c69
Leo Laporte <leo@leoville.com>
2022-02-12 18:47:49 -0800

    Day 17.2 the struggle is real

--- AOC21/day17.rkt
+++ AOC21/day17.rkt
@@ -61,8 +61,7 @@ First instinct is to brute force it. Let's see how fast that is.
 - a function, step, that advances the probe one step.
 - a function, flight, that repeats until probe hits the target or passes out of reach.
 - a boolean, hit-target?, that tells us whether the probe has hit the target
-- a boolean, in-range? that tells us whether the probe is out of range (stop IOW)
-- a hash, hits, with starting velocity as the key and max-y as the value.
+- a boolean, out-of-range? that tells us whether the probe is out of range
 
 Only one more question: what range of velocities should I try? I don't want to
 search an infinite set. Ah this is what makes it not entirely brute force. Can I
@@ -89,14 +88,14 @@ Part 2 needs to know more.
 #|                                     CODE                                     |#
 #|==============================================================================|#
 
+(define (sum-of-numbers n)
+  "the sum of a number series ending in n = (n * (n + 1)) / 2"
+  (/ (* n (add1 n)) 2))    ; triangle numbers
+
 ;; Target -> Natural
 ;; given a target calculate the maximum height a probe
 ;; can reach and still hit the target
 (define (max-y-height t)
-  "the sum of a number series ending in n = (n * (n + 1)) / 2"
-  (define (sum-of-numbers n)
-    (/ (* n (add1 n)) 2))    ; triangle numbers
-
   ; how low can we go? that defines how high we can fly
   (sum-of-numbers (- (- (target-y-min t)) 1)))
 
@@ -116,6 +115,18 @@ area after any step?
 
 ==================================================================================|#
 
+#|
+Two ways to go about this. One is my original plan to try all the possibilities
+and count up the hits. Given reasonable boundaries on the x and y velocities
+this is pretty quick.
+
+The other way - inspired by the backwards way I solved part one - is to calculate
+the set of triangle numbers for x and y and find the intersection with the set
+of points in the target. That should work too.
+
+I've already written the code for method one:
+
+|#
 (struct probe (x y xv yv) #:transparent) ; contains the current state of the probe
 
 ;; Probe -> Probe
@@ -131,15 +142,14 @@ area after any step?
    (+ (probe-y p) (probe-yv p))
        
    ; new x-velocity
-   (if (> (probe-xv p) 0)
-       (sub1 (probe-xv p))
-       0)
+   (sub1 (probe-xv p)) 
+
      
    ; new y-velocity
    (sub1 (probe-yv p))))
 
 (module+ test
-  (check-equal? (step (probe 0 0 0 0)) (probe 0 0 0 -1))
+  (check-equal? (step (probe 0 0 0 0)) (probe 0 0 -1 -1))
   (check-equal? (step (probe 1 2 7 8)) (probe 8 10 6 7))
   (check-equal? (step (probe 0 -2 7 8)) (probe 7 6 6 7))
   (check-equal? (step (probe 1 -2 7 -8)) (probe 8 -10 6 -9)))
@@ -156,60 +166,55 @@ area after any step?
 
 ;; Probe Target -> Boolean
 ;; returns true if it's still possible to hit target
-(define (in-range? p t)
-  (and (<= (probe-x p) (target-x-max t))   
-       (>= (probe-y p) (target-y-max t)))) 
+(define (out-of-range? p t)
+ (or (> (probe-x p) (target-x-max t))
+     (< (probe-xv p) -1000)))
 
 (module+ test
-  (check-equal? (in-range? (probe 5 -9 0 0) (target 20 30 -5 -10)) #t)
-  (check-equal? (in-range? (probe 26 -11 0 0) (target 20 30 -5 -10)) #f)
-  (check-equal? (in-range? (probe 26 -9 0 0) (target 20 30 -5 -10)) #t))
+  (check-equal? (out-of-range? (probe 31 -9 0 0) (target 20 30 -5 -10)) #t)
+  (check-equal? (out-of-range? (probe 26 -11 0 0) (target 20 30 -5 -10)) #f))
 
 ;; Probe -> Natural or #f
 ;; Given a probe return #t if it hits the target 
 ;; #f if it misses
 (define (flight p t)
   (cond [(hit-target? p t) #t]
-        [(not (in-range? p t)) #f]
+        [(out-of-range? p t) #f]
         [else (flight (step p) t)]))
 
-#|
- (trace flight)
- (flight (probe 0 0 7 8) (make-target "target area: x=20..30, y=-10..-5"))
 
-(regexp-match* #px"(-?\\d+),(-?\\d+)" str)
-
-"23,-10  25,-9   27,-5   29,-6   22,-6   21,-7   9,0     27,-7   24,-5
-25,-7   26,-6   25,-5   6,8     11,-2   20,-5   29,-10  6,3     28,-7
-8,0     30,-6   29,-8   20,-10  6,7     6,4     6,1     14,-4   21,-6
-26,-10  7,-1    7,7     8,-1    21,-9   6,2     20,-7   30,-10  14,-3
-20,-8   13,-2   7,3     28,-8   29,-9   15,-3   22,-5   26,-8   25,-8
-25,-6   15,-4   9,-2    15,-2   12,-2   28,-9   12,-3   24,-6   23,-7
-25,-10  7,8     11,-3   26,-7   7,1     23,-9   6,0     22,-10  27,-6
-8,1     22,-8   13,-4   7,6     28,-6   11,-4   12,-4   26,-9   7,4
-24,-10  23,-8   30,-8   7,0     9,-1    10,-1   26,-5   22,-9   6,5
-7,5     23,-6   28,-10  10,-2   11,-1   20,-9   14,-2   29,-7   13,-3
-23,-5   24,-8   27,-9   30,-7   28,-5   21,-10  7,9     6,6     21,-5
-27,-10  7,2     30,-9   21,-8   22,-7   24,-9   20,-6   6,9     29,-5
-8,-2    27,-8   30,-5   24,-7"
-|#
-#|
-So yeah, everything I wrote in part 1 is still good here. The question remains,
-what should the range of values for xv and yv be? In the sample problem the range
-is xv: 6..30, yv: 10...-7 - I could just guess.
-|#
+(trace flight)
+(flight (probe 0 0 7 8) (make-target "target area: x=20..30, y=-10..-5"))
 
 ;; Natural Natural Natural Natural Target -> Natural
 ;; given the minimum and maximum velocities for x and y
 ;; return the number of successful flights
 (define (test-flights xv-min xv-max yv-min yv-max t)
   (for*/fold ([hits 0])
-            ([xv (in-range xv-min (add1 xv-max))]
-             [yv (in-range yv-min (add1 yv-max))])
+             ([xv (in-range xv-min (add1 xv-max))]
+              [yv (in-range yv-min (add1 yv-max))])
     (values (+ hits
                (if (flight (probe 0 0 xv yv) t)
                    1
                    0)))))
+
+#|
+This will be way too slow unless I can calculate reasonable
+ranges for xv and yv. 
+|#
+
+;; Naural -> Hash
+;; given a range of integers, return a hash of the
+;; step number and its triangle number
+(define (step-v-hash velocity steps)
+  (for/hash ([step (in-range steps)]
+             [v (in-range velocity -1000 -1)])
+    (values step (sum-of-numbers v))))
+
+(define (velo edge v)
+  (cond [(< edge 0) v]
+        [else (velo (- edge v) (add1 v))]))
+
 
 (define (day17.2 str)
   (test-flights 0 100 -100 100 (make-target str)))
