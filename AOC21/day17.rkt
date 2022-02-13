d8c2da26f4f78bb472045e9f1481e94dc94804a2
Leo Laporte <leo@leoville.com>
2022-02-12 23:27:49 -0800

    Day 17 solved - with a couple of guesses

--- AOC21/day17.rkt
+++ AOC21/day17.rkt
@@ -124,7 +124,9 @@ The other way - inspired by the backwards way I solved part one - is to calculat
 the set of triangle numbers for x and y and find the intersection with the set
 of points in the target. That should work too.
 
-I've already written the code for method one:
+I've already written the code for method one so let's see if we can get it fast
+enuf. (Turns out it's plenty fast so I'll leave the second method to another
+time.)
 
 |#
 (struct probe (x y xv yv) #:transparent) ; contains the current state of the probe
@@ -137,17 +139,10 @@ I've already written the code for method one:
    (if (<= (probe-xv p) 0)           ; velocity has bottomed out
        (probe-x p)                   ; so we ain't movin'
        (+ (probe-x p) (probe-xv p))) ; otherwise move by x velocity
-
-   ; new y-pos 
-   (+ (probe-y p) (probe-yv p))
-       
-   ; new x-velocity
-   (sub1 (probe-xv p)) 
-
-     
-   ; new y-velocity
-   (sub1 (probe-yv p))))
-
+   (+ (probe-y p) (probe-yv p))      ; new y-pos 
+   (sub1 (probe-xv p))               ; new x-velocity
+   (sub1 (probe-yv p))))             ; new y-velocity
+  
 (module+ test
   (check-equal? (step (probe 0 0 0 0)) (probe 0 0 -1 -1))
   (check-equal? (step (probe 1 2 7 8)) (probe 8 10 6 7))
@@ -165,10 +160,10 @@ I've already written the code for method one:
   (check-equal? (hit-target? (probe 10 20 0 0) (target 20 25 20 25)) #f))
 
 ;; Probe Target -> Boolean
-;; returns true if it's still possible to hit target
+;; returns true if target can no longer be reached
 (define (out-of-range? p t)
- (or (> (probe-x p) (target-x-max t))
-     (< (probe-xv p) -1000)))
+  (or (> (probe-x p) (target-x-max t))
+      (< (probe-y p) -100)))  ; this number is a wild guess
 
 (module+ test
   (check-equal? (out-of-range? (probe 31 -9 0 0) (target 20 30 -5 -10)) #t)
@@ -181,10 +176,6 @@ I've already written the code for method one:
   (cond [(hit-target? p t) #t]
         [(out-of-range? p t) #f]
         [else (flight (step p) t)]))
-
-
-(trace flight)
-(flight (probe 0 0 7 8) (make-target "target area: x=20..30, y=-10..-5"))
 
 ;; Natural Natural Natural Natural Target -> Natural
 ;; given the minimum and maximum velocities for x and y
@@ -200,32 +191,53 @@ I've already written the code for method one:
 
 #|
 This will be way too slow unless I can calculate reasonable
-ranges for xv and yv. 
+ranges for xv and yv.
+
+xv isn't too hard: vx-min = the lowest velocity that gets to the target,
+and vx-max = right edge of the target (any higher and the first shot
+will go right past the target).
+
+I'll have to think about what vy-max should be. I'm sure there's
+a way. Meanwhile I'll just plug in -100 .. 100
 |#
 
-;; Naural -> Hash
-;; given a range of integers, return a hash of the
-;; step number and its triangle number
-(define (step-v-hash velocity steps)
-  (for/hash ([step (in-range steps)]
-             [v (in-range velocity -1000 -1)])
-    (values step (sum-of-numbers v))))
+;; Natural Natural -> Natural
+;; Given a target point and a velocity calculate
+;; the lowest starting velocity at point 0 that
+;; will get to the target - this starts at the left
+;; edge of the target and works backward from a
+;; velocity of zero by adding 1 each step until
+;; it goes past the 0 point.
+(define (minimum-x-velocity target velocity)
+  (cond [(<= target 0) (sub1 velocity)]
+        [else (minimum-x-velocity (- target velocity) (add1 velocity))]))
 
-(define (velo edge v)
-  (cond [(< edge 0) v]
-        [else (velo (- edge v) (add1 v))]))
-
+#|
+The yv range is another matter. It can be negative or positive.
+yv-min is the number that would hit the target
+in one shot, (target-y-min t). Not sure how to figure the highest
+so I'll choose 100 and see if it works. It does!
+|#
 
 (define (day17.2 str)
-  (test-flights 0 100 -100 100 (make-target str)))
+  (let ([t (make-target str)])
+    (test-flights (minimum-x-velocity (target-x-min t) 0)
+                  (add1 (target-x-max t)) 
+                  (target-y-min t)        ; velocity that gets there in 1 step
+                  100 ; completely arbitrary fudge factor
+                  t)))
 
 (module+ test
   (check-equal? (day17.2 "target area: x=20..30, y=-10..-5") 112))
 
-; (time (printf "2021 AOC Problem 17.2 = ~a\n" (day16.2 input)))
+(time (printf "2021 AOC Problem 17.2 = ~a\n" (day17.2 (file->string "input17.txt"))))
 
 #|
 Time to solve, in milliseconds, on a 2021 M1 Pro MacBook Pro 14" with 16GB RAM
 
+2021 AOC Problem 16.1 = 3570
+cpu time: 0 real time: 0 gc time: 0
+2021 AOC Problem 17.2 = 1919
+cpu time: 65 real time: 68 gc time: 7
 
 |#
