a092b2e9c7af9911386560d5e4132814d9b99259
Leo Laporte <leo@leoville.com>
2022-02-27 10:30:09 -0800

    Clean up - shorten problem descriptions

--- AOC21/day03.rkt
+++ AOC21/day03.rkt
@@ -1,61 +1,30 @@
 #lang racket
-;  AOC 2021
-; Leo Laporte 3 Dec 2021
-; 
-; --- Day 3: Binary Diagnostic ---
-; 
-; The submarine has been making some odd creaking noises, so you ask it to produce a
-; diagnostic report just in case.
-; 
-; The diagnostic report (your puzzle input) consists of a list of binary numbers
-; which, when decoded properly, can tell you many useful things about the conditions
-; of the submarine. The first parameter to check is the power consumption.
-; 
-; You need to use the binary numbers in the diagnostic report to generate two new
-; binary numbers (called the gamma rate and the epsilon rate). The power consumption
-; can then be found by multiplying the gamma rate by the epsilon rate.
-; 
-; Each bit in the gamma rate can be determined by finding the most common bit in
-; the corresponding position of all numbers in the diagnostic report. For example,
-; given the following diagnostic report:
-; 
-; 00100
-; 11110
-; 10110
-; 10111
-; 10101
-; 01111
-; 00111
-; 11100
-; 10000
-; 11001
-; 00010
-; 01010
-; 
-; Considering only the first bit of each number, there are five 0 bits and seven 1 bits.
-; Since the most common bit is 1, the first bit of the gamma rate is 1.
-; 
-; The most common second bit of the numbers in the diagnostic report is 0, so the second
-; bit of the gamma rate is 0.
-; 
-; The most common value of the third, fourth, and fifth bits are 1, 1, and 0, respectively,
-; and so the final three bits of the gamma rate are 110.
-; 
-; So, the gamma rate is the binary number 10110, or 22 in decimal.
-; 
-; The epsilon rate is calculated in a similar way; rather than use the most common bit,
-; the least common bit from each position is used. So, the epsilon rate is 01001, or 9 in
-; decimal. Multiplying the gamma rate (22) by the epsilon rate (9) produces the power
-; consumption, 198.
-; 
-; Use the binary numbers in your diagnostic report to calculate the gamma rate and
-; epsilon rate, then multiply them together. What is the power consumption of the submarine?
-; (Be sure to represent your answer in decimal, not binary.)
-; 
-; 
+
+#|
+  AOC 2021
+ Leo Laporte 3 Dec 2021
+ 
+ --- Day 3: Binary Diagnostic ---
+
+ You need to use the binary numbers in the diagnostic report to generate two new
+ binary numbers (called the gamma rate and the epsilon rate). The power consumption
+ can then be found by multiplying the gamma rate by the epsilon rate.
+ 
+ Each bit in the gamma rate can be determined by finding the most common bit in
+ the corresponding position of all numbers in the diagnostic report.
+ 
+ The epsilon rate is calculated in a similar way; rather than use the most common bit,
+ the least common bit from each position is used.
+
+ Use the binary numbers in your diagnostic report to calculate the gamma rate and
+ epsilon rate, then multiply them together.
+
+ |#
 
 
-(require racket/file threading rackunit)
+(require racket/file
+         threading
+         rackunit)
 
 ;; Problem input from adventofcode.com
 (define day3data (string-split (file->string "input3.txt") "\n"))
@@ -63,11 +32,16 @@
 (define sample-data
   (list "00100" "11110" "10110" "10111" "10101" "01111" "00111" "11100" "10000" "11001" "00010" "01010"))
 
-;; naive method
-;; 1. add up each place of each number in the list of binary numbers 
-;; 2. per place divide place total by 2, if greater than length of list, that place is 1, else 0
-;; 3. multiply that number represented by all places (gamma) by its ones-complement (epsilon), result is solution
-;; this is so heinous there must be a better way but I can't think of what it is
+#|
+NOTES:
+
+ naive method
+ 1. add up each place of each number in the list of binary numbers 
+ 2. per place divide place total by 2, if greater than length of list, that place is 1, else 0
+ 3. multiply that number represented by all places (gamma) by its ones-complement (epsilon), result is solution
+ this is so heinous there must be a better way but I can't think of what it is
+
+|#
 
 ;; list-of string -> integer
 ;; given a list of strings of binary numbers produce the product of the gamma and epsilon
@@ -109,6 +83,7 @@
 ;; given a list of numbers, return the gamma and epsilon values
 (define (sum-row len total-list)
   (let ([threshold (quotient len 2)]) ; if sum of digits > len/2 then there are more 1s than 0s
+    
     (for/fold ([gamma null]       ; accumulators...
                [epsilon null]     ; ... fold builds lists of 1s and 0s
                #:result (cons (bin-list->decimal (reverse gamma)) (bin-list->decimal (reverse epsilon)))) 
@@ -145,68 +120,21 @@
 
 (time (printf "2021 AOC Problem 3.1 = ~a\n" (day3.1 day3data)))
 
-;  --- Part Two ---
-; 
-; Next, you should verify the life support rating, which can be determined by multiplying
-; the oxygen generator rating by the CO2 scrubber rating.
-; 
-; Both the oxygen generator rating and the CO2 scrubber rating are values that can be found
-; in your diagnostic report - finding them is the tricky part. Both values are located using
-; a similar process that involves filtering out values until only one remains. Before searching
-; for either rating value, start with the full list of binary numbers from your diagnostic
-; report and consider just the first bit of those numbers. Then:
-; 
-; Keep only numbers selected by the bit criteria for the type of rating value for which you
-; are searching. Discard numbers which do not match the bit criteria.
-; If you only have one number left, stop; this is the rating value for which you are searching.
-; Otherwise, repeat the process, considering the next bit to the right.
-; The bit criteria depends on which type of rating value you want to find:
-; 
-; To find oxygen generator rating, determine the most common value (0 or 1) in the current bit
-; position, and keep only numbers with that bit in that position. If 0 and 1 are equally common,
-; keep values with a 1 in the position being considered.
-; 
-; To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit
-; position, and keep only numbers with that bit in that position. If 0 and 1 are equally common,
-; keep values with a 0 in the position being considered.
-; 
-; For example, to determine the oxygen generator rating value using the same example diagnostic
-; report from above:
-; 
-; Start with all 12 numbers and consider only the first bit of each number.
-; There are more 1 bits (7) than 0 bits (5), so keep only the 7 numbers with a 1 in the
-; first position: 11110, 10110, 10111, 10101, 11100, 10000, and 11001.
-; 
-; Then, consider the second bit of the 7 remaining numbers: there are more 0 bits (4)
-; than 1 bits (3), so keep only the 4 numbers with a 0 in the
-; second position: 10110, 10111, 10101, and 10000.
-; 
-; In the third position, three of the four numbers have a 1, so keep those three:
-; 10110, 10111, and 10101.
-; 
-; In the fourth position, two of the three numbers have a 1, so keep those two: 10110 and 10111.
-; 
-; In the fifth position, there are an equal number of 0 bits and 1 bits (one each). So, to find
-; the oxygen generator rating, keep the number with a 1 in that position: 10111.
-; 
-; As there is only one number left, stop; the oxygen generator rating is 10111, or 23 in decimal.
-; Then, to determine the CO2 scrubber rating value from the same example above:
-; 
-; Start again with all 12 numbers and consider only the first bit of each number.
-; There are fewer 0 bits (5) than 1 bits (7), so keep only the 5 numbers with a 0 in the
-; first position: 00100, 01111, 00111, 00010, and 01010.
-; Then, consider the second bit of the 5 remaining numbers: there are fewer 1 bits (2) than
-; 0 bits (3), so keep only the 2 numbers with a 1 in the second position: 01111 and 01010.
-; In the third position, there are an equal number of 0 bits and 1 bits (one each). So, to
-; find the CO2 scrubber rating, keep the number with a 0 in that position: 01010.
-; As there is only one number left, stop; the CO2 scrubber rating is 01010, or 10 in decimal.
-; Finally, to find the life support rating, multiply the oxygen generator rating (23) by the
-; CO2 scrubber rating (10) to get 230.
-; 
-; Use the binary numbers in your diagnostic report to calculate the oxygen generator rating and
-; CO2 scrubber rating, then multiply them together. What is the life support rating of the
-; submarine? (Be sure to represent your answer in decimal, not binary.)
-; 
+#|
+  --- Part Two ---
+
+ To find oxygen generator rating, determine the most common value (0 or 1) in the current bit
+ position, and keep only numbers with that bit in that position. If 0 and 1 are equally common,
+ keep values with a 1 in the position being considered.
+ 
+ To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit
+ position, and keep only numbers with that bit in that position. If 0 and 1 are equally common,
+ keep values with a 0 in the position being considered.
+ 
+ Use the binary numbers in your diagnostic report to calculate the oxygen generator rating and
+ CO2 scrubber rating, then multiply them together. What is the life support rating of the
+ submarine? (Be sure to represent your answer in decimal, not binary.)
+|#
 
 
 (define O2 1)
@@ -278,17 +206,21 @@
 (module+ test
   (check-equal? (day3.2 sample-data) 230))
 
- (time (printf "2021 AOC Problem 3.2 = ~a\n" (day3.2 day3data)))
+(time (printf "2021 AOC Problem 3.2 = ~a\n" (day3.2 day3data)))
 
-; Time to solve, in milliseconds, on a 2021 M1 Pro MacBook Pro 14" with 16GB RAM
-; 2021 AOC Problem 3.1 = 3885894
-; cpu time: 3 real time: 3 gc time: 0
-; 2021 AOC Problem 3.2 = 4375225
-; cpu time: 2 real time: 2 gc time: 0
+#|
+ Time to solve, in milliseconds, on a 2021 M1 Pro MacBook Pro 14" with 16GB RAM
 
-; Real world timing
-;      --------Part 1---------   --------Part 2--------
-;Day       Time    Rank  Score       Time   Rank  Score
-;  3       >24h  102352      0       >24h  88448      0
-;  2   00:38:41   13051      0   01:06:26  14647      0
-;  1   00:24:22    7349      0   12:27:40  59726      0
+2021 AOC Problem 3.1 = 3885894
+cpu time: 0 real time: 0 gc time: 0
+2021 AOC Problem 3.2 = 4375225
+cpu time: 0 real time: 0 gc time: 0
+
+ Real world timing
+
+      --------Part 1---------   --------Part 2--------
+Day       Time    Rank  Score       Time   Rank  Score
+  3       >24h  102352      0       >24h  88448      0
+  2   00:38:41   13051      0   01:06:26  14647      0
+  1   00:24:22    7349      0   12:27:40  59726      0
+|#
